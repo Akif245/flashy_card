9a0f31b291b646be3f52b3e77a4976ca734c3ce8
Akif245 <abdulakif44570@gmail.com>
2025-04-27 00:14:43 +0530

    images setup

--- flash.py
+++ flash.py
@@ -10,8 +10,18 @@ cardimg=PhotoImage(file="card_front.png")
 canvas.create_image(400,263,image=cardimg)
 canvas.config(bg=bgc,highlightthickness=0)
 canvas.create_text(400,150,text="Hello",font=("Ariel",40))
-canvas.grid(row=0,column=0)
+canvas.create_text(400,263,text="Word",font=("Ariel",60,"bold"))
+canvas.grid(row=0,column=0,columnspan=2)
 
+
+
+cross=PhotoImage(file="wrong.png")
+btn1=Button(image=cross,highlightthickness=0)
+btn1.grid(row=1,column=0)
+
+correct=PhotoImage(file="right.png")
+btn2=Button(image=correct,highlightthickness=0)
+btn2.grid(row=1,column=1)
 
 
 
